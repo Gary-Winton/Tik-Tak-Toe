30524d95d78531aeb1f1410d8a63bacf510f1cf2
Gary <gary-winton@live.nmit.ac.nz>
2020-06-22 12:16:59 +1200

    full game

--- index.js
+++ index.js
@@ -1,5 +1,68 @@
-[
-    ["x", "0", "0"];
-    ["0", "0", "x"];
-    ["x", "x", "x"] 
-  ]
+
+const ttt = [
+    ["X", "O", "X"],
+    ["X", "X", "O"],
+    ["O", "X", "O"] 
+  ];
+
+  const ifEmpty = function() {
+    if ((ttt[0][0]=== "E" && ttt[0][1]=== "E" && ttt[0][2]=== "E"  )&&
+       (ttt[1][0]=== "E" && ttt[1][1]=== "E" && ttt[1][2]=== "E"  )&&
+       (ttt[2][0]=== "E" && ttt[2][1]=== "E" && ttt[2][2]=== "E"  )){
+      return "start a game"
+    }
+  }
+
+console.log(ifEmpty());
+
+const progress = function() {
+  if ((ttt[0][0]=== "E" || ttt[0][1]=== "E" || ttt[0][2]=== "E"  ) ||
+     (ttt[1][0]=== "E" || ttt[1][1]=== "E" || ttt[1][2]=== "E"  )||
+     (ttt[2][0]=== "E" || ttt[2][1]=== "E" || ttt[2][2]=== "E"  )){
+    return "game as started"
+  }
+}
+
+console.log(progress());
+
+const  winx= function() {
+  if ((ttt[0][0]=== "X" && ttt[0][1]=== "X" && ttt[0][2]=== "X"  ) ||
+      (ttt[1][0]=== "X" && ttt[1][1]=== "X" && ttt[1][2]=== "X"  ) ||
+      (ttt[2][0]=== "X" && ttt[2][1]=== "X" && ttt[2][2]=== "X"  ) ||
+      (ttt[0][0]=== "X" && ttt[1][0]=== "X" && ttt[2][0]=== "X"  ) ||
+      (ttt[0][1]=== "X" && ttt[1][1]=== "X" && ttt[2][1]=== "X"  ) ||
+      (ttt[0][2]=== "X" && ttt[1][2]=== "X" && ttt[2][2]=== "X"  ) ||
+      (ttt[0][0]=== "X" && ttt[1][1]=== "X" && ttt[2][2]=== "X"  ) ||
+      (ttt[0][2]=== "X" && ttt[1][1]=== "X" && ttt[2][0]=== "X"  )){
+    return "game x wins"
+  }
+}
+
+console.log(winx());
+
+const  winO= function() {
+  if ((ttt[0][0]=== "O" && ttt[0][1]=== "O" && ttt[0][2]=== "O"  ) ||
+      (ttt[1][0]=== "O" && ttt[1][1]=== "O" && ttt[1][2]=== "O"  ) ||
+      (ttt[2][0]=== "O" && ttt[2][1]=== "O" && ttt[2][2]=== "O"  ) ||
+      (ttt[0][0]=== "O" && ttt[1][0]=== "O" && ttt[2][0]=== "O"  ) ||
+      (ttt[0][1]=== "O" && ttt[1][1]=== "O" && ttt[2][1]=== "O"  ) ||
+      (ttt[0][2]=== "O" && ttt[1][2]=== "O" && ttt[2][2]=== "O"  ) ||
+      (ttt[0][0]=== "O" && ttt[1][1]=== "O" && ttt[2][2]=== "O"  ) ||
+      (ttt[0][2]=== "O" && ttt[1][1]=== "O" && ttt[2][0]=== "O"  )){
+    return "game O wins"
+  }
+}
+
+console.log(winO());
+
+
+const draw = function(){
+if ((ifEmpty()===undefined)&&
+   (progress()===undefined)&&
+   (winx()===undefined)&&
+   (winO()===undefined))
+{
+  return "draw"
+}
+}
+console.log(draw());
